caa1a6f40073615a5e30b6be992a4df6303db032
= <loic.delpierre16@gmail.com>
2021-11-15 14:59:12 +0100

    modif

--- Joueur.java
+++ Joueur.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class Joueur {
+
+    public Joueur(){
+
+    }
+    public String toString(){
+        return "joueur";
+    }
+
+    public void updateScore(){
+
+    }
+
+    public void changementEtat(){
+
+    }
 }
--- Phase.java
+++ Phase.java
@@ -1,4 +1,8 @@
 package com.company;
 
 public interface Phase {
+
+    void selectionerJoueurs();
+
+    void déroulerPhase();
 }
